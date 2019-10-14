1f8f036b11f17851aca02098863711a8de57fd2b
jan.wiegmann <jan.wiegmann@openknowledge.de>
2019-10-14 17:50:42 +0200

    Repo flush

--- backend/src/main/java/de/openknowledge/jwe/infrastructure/constants/Constants.java
+++
@@ -1,8 +0,0 @@
-package de.openknowledge.jwe.infrastructure.constants;
-
-public class Constants {
-
-  public static final String ROOT_SERVICE_URI = "service";
-    public static final String TWEETS_API_URI = "tweets";
-    public static final String USERS_API_URI = "users";
-}
--- backend/src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO tab_user(user_name, user_password) VALUES ('jw', 'password');
-INSERT INTO tab_user(user_name, user_password) VALUES ('foo', 'password');
-INSERT INTO tab_user_role_relationship(user_id, user_role) VALUES ('1', 'USER');
-INSERT INTO tab_user_role_relationship(user_id, user_role) VALUES ('2', 'USER');
